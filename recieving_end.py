a6d7fed89a4e73f66ff225f55cf7b0d570eb4d29
abdullah2891 <abdullah2891@gmail.com>
2015-08-16 09:07:57 +0600

    Removed some unnecessary part of the code

--- recieving_end.py
+++ recieving_end.py
@@ -6,15 +6,13 @@ def xbee_reading():
         Port=sys.argv[1]
         #Port='COM3'
         buffer_clear=0
-        p=0;
-        q=0;
+
         ser = serial.Serial(port=Port,baudrate=115200)
         counter=0
         string=""
-        msb=0;
+
         lsb=0;
-        formatter=0
-        reading=0
+
 
         a=time.time()
         while True and counter<52  :
@@ -23,7 +21,6 @@ def xbee_reading():
                 #print(n)
                 if(n==126):
                     counter=0;
-                    #print "---------- Timestamp: "+str(time.time())+"-----------------"
                     buffer_clear=0
                     formatter=0
                                    
@@ -32,7 +29,6 @@ def xbee_reading():
                     
             
                 if(counter>=19):
-                        #print str(n)+"    "+str(counter)
                         rem=(counter-18)%2
                         square=(counter-18)%8
 
@@ -45,25 +41,13 @@ def xbee_reading():
                                         reading=5.0*(reading/1023.0)
                                         string+=str(reading)+" ;"#+str(buffer_clear)+" "
                                         buffer_clear+=1
-                        #if(square==0):
-                         #       string+=";"
-        #print len(string)
+
                         
                         
         b=time.time()
 
 
-                
-        #file = open("newfile.txt", "w")
 
-        #file.write(string)
-        #file.close()
-
-                        
-        #output="1 2 ;3 4"
-
-        #print string
-        #print b-a
         
         ser.close()
 
